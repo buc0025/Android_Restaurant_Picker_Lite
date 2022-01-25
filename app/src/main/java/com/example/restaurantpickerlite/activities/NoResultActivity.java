2b68f4f7012a30596695f1c88ac9874056932425
buc0025 <stanley.yang001@gmail.com>
2022-01-24 20:16:40 -0500

    Added activity for no results picking randomly

---
+++ app/src/main/java/com/example/restaurantpickerlite/activities/NoResultActivity.java
@@ -0,0 +1,16 @@
+package com.example.restaurantpickerlite.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.restaurantpickerlite.R;
+
+public class NoResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_no_result);
+    }
+}
