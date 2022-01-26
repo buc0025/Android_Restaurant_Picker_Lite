b7f45fda60902ecf4bda01b5e41ef5d90c183e09
buc0025 <stanley.yang001@gmail.com>
2022-01-26 18:51:49 -0500

    Back button goes to main activity from no results activity

--- app/src/main/java/com/example/restaurantpickerlite/activities/NoResultActivity.java
+++ app/src/main/java/com/example/restaurantpickerlite/activities/NoResultActivity.java
@@ -2,6 +2,7 @@ package com.example.restaurantpickerlite.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.restaurantpickerlite.R;
@@ -12,5 +13,14 @@ public class NoResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_no_result);
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(NoResultActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
