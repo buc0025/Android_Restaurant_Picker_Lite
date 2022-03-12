f79834df6e885e18567faba3fcab88db4611e377
buc0025 <stanley.yang001@gmail.com>
2022-03-11 21:35:27 -0500

    Adjusted text size and style of spinner and working on spacing for buttons

--- app/src/main/java/com/example/restaurantpickerlite/activities/MainActivity.java
+++ app/src/main/java/com/example/restaurantpickerlite/activities/MainActivity.java
@@ -71,11 +71,11 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         // Creating adapter for spinner
         spinnerMilesRadius = findViewById(R.id.spinnerMilesRadius);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.milesRadius, android.R.layout
-                .simple_spinner_item);
+//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.milesRadius, android.R.layout
+//                .simple_spinner_item);
 
-//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.milesRadius, R.layout.spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.milesRadius, R.layout.spinner_item);
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerMilesRadius.setAdapter(adapter);
         spinnerMilesRadius.setOnItemSelectedListener(this);
 
